bde5c36e56d87fbd4bebb0ea1227cdf309d5eda7
Aetias <aetias@outlook.com>
2024-10-12 12:23:05 +0200

    `import`: Add switch to import default symbol names

--- src/cmd/import/symbols.rs
+++ src/cmd/import/symbols.rs
@@ -1,7 +1,4 @@
-use std::{
-    io::{stdout, Write},
-    path::PathBuf,
-};
+use std::{borrow::Cow, path::PathBuf};
 
 use anyhow::Result;
 use argp::FromArgs;
@@ -24,6 +21,14 @@ pub struct ImportSymbols {
     /// Path to built/linked ELF file.
     #[argp(option, short = 'x')]
     elf_path: PathBuf,
+
+    /// Includes symbols with default names like `func_ov12_0211514c`.
+    #[argp(switch, short = 'D')]
+    include_default_names: bool,
+
+    /// Dry run, do not write any files.
+    #[argp(switch, short = 'd')]
+    dry: bool,
 }
 
 impl ImportSymbols {
@@ -36,23 +41,19 @@ impl ImportSymbols {
         let file = read_file(&self.elf_path)?;
         let object = object::File::parse(&*file)?;
 
-        let mut stdout = stdout().lock();
-
         for section in object.sections() {
             let section_name = section.name()?;
-            writeln!(stdout, "Section: {section_name}")?;
+            log::debug!("Section: {section_name}");
             let Some(module_kind) = self.parse_module_kind(section_name)? else { continue };
             let symbol_map = symbol_maps.get_mut(module_kind);
-            writeln!(stdout, "Module: {module_kind}")?;
+            log::debug!("Module: {module_kind}");
             for symbol in object.symbols() {
                 if symbol.section_index() != Some(section.index()) {
                     continue;
                 };
 
                 let name = symbol.name()?;
-                if name.starts_with("func")
-                    || name.starts_with("data")
-                    || name.starts_with(".")
+                if name.starts_with(".")
                     || name.starts_with("$")
                     || name.starts_with("ov")
                     || name.starts_with("arm9")
@@ -63,18 +64,34 @@ impl ImportSymbols {
                     continue;
                 }
 
+                let is_default_name = name.starts_with("func_") || name.starts_with("data_");
+
+                let name = if is_default_name {
+                    if !self.include_default_names {
+                        continue;
+                    } else {
+                        self.pad_default_symbol(name)?
+                    }
+                } else {
+                    name.into()
+                };
+
                 let address = symbol.address() as u32;
                 if address == 0 {
                     continue;
                 }
 
-                writeln!(stdout, "{address:#x} {name}")?;
-
-                symbol_map.rename_by_address(address, name)?;
+                let result = symbol_map.rename_by_address(address, &name);
+                if !is_default_name {
+                    log::debug!("{address:#x} {name}");
+                    result?;
+                }
             }
         }
 
-        symbol_maps.to_files(&config, &self.config_path.parent().unwrap())?;
+        if !self.dry {
+            symbol_maps.to_files(&config, &self.config_path.parent().unwrap())?;
+        }
 
         Ok(())
     }
@@ -92,5 +109,28 @@ impl ImportSymbols {
         } else {
             Ok(None)
         }
+    }
+
+    fn pad_default_symbol<'a>(&self, name: &'a str) -> Result<Cow<'a, str>> {
+        let split = name.split('_').collect::<Vec<_>>();
+        if split.len() <= 2 {
+            return Ok(name.into());
+        }
+
+        let module_name = split[1];
+        let Some(overlay_number) = module_name.strip_prefix("ov") else {
+            return Ok(name.into());
+        };
+
+        if overlay_number.len() >= 3 || !overlay_number.chars().next().unwrap().is_ascii_digit() {
+            return Ok(name.into());
+        }
+
+        let overlay_id: u16 = overlay_number.parse()?;
+
+        let prefix = split[0];
+        let suffix = split[2..].join("_");
+
+        Ok(format!("{prefix}_ov{overlay_id:03}_{suffix}").into())
     }
 }
