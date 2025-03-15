bc27e06fd298aafb9214cd99a7479deb738d899c
Aetias <aetias@outlook.com>
2025-03-15 15:12:25 +0100

    Generate symbols for overlay IDs

--- cli/src/cmd/lcf.rs
+++ cli/src/cmd/lcf.rs
@@ -200,6 +200,12 @@ impl Lcf {
         };
 
         writeln!(lcf, "    {module_name} : {{")?;
+        writeln!(lcf, "        ALIGNALL(4);")?;
+        if let ModuleKind::Overlay(id) = module_kind {
+            let name = &module.name;
+            writeln!(lcf, "        OVERLAY_{id}_ID = {id};")?;
+            writeln!(lcf, "        OVERLAY_{name}_ID = {id};")?;
+        }
         let delinks = Delinks::from_file_and_generate_gaps(config_dir.join(&module.delinks), module_kind)?;
         for section in delinks.sections.sorted_by_address() {
             writeln!(lcf, "        . = ALIGN({});", section.alignment())?;
