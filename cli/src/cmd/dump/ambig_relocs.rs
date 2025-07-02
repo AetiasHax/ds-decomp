0f6270326e366e7e3a9064771b4f1dc1ac4f5e63
Aetias <aetias@outlook.com>
2025-07-02 22:19:38 +0200

    dump ambig_relocs: Print module kind

--- cli/src/cmd/dump/ambig_relocs.rs
+++ cli/src/cmd/dump/ambig_relocs.rs
@@ -22,14 +22,20 @@ impl DumpAmbigRelocs {
         let config_path = self.config_path.parent().unwrap();
 
         let ambig_relocs = self.get_all_ambiguous_relocations(config_path, &config)?;
-        for reloc in &ambig_relocs {
-            let symbol_info = if let Some(symbol) = &reloc.symbol {
+        for reloc_info in &ambig_relocs {
+            let symbol_info = if let Some(symbol) = &reloc_info.symbol {
                 let name = demangle(&symbol.name);
-                format!("{} + {:#x}", name, reloc.offset)
+                format!("{} + {:#x}", name, reloc_info.offset)
             } else {
                 "<no symbol>".to_string()
             };
-            println!("{:#010x} -> {:#010x} ({})", reloc.relocation.from_address(), reloc.relocation.to_address(), symbol_info);
+            println!(
+                "{}: {:#010x} -> {:#010x} ({})",
+                reloc_info.module_kind,
+                reloc_info.relocation.from_address(),
+                reloc_info.relocation.to_address(),
+                symbol_info
+            );
         }
 
         Ok(())
@@ -42,19 +48,24 @@ impl DumpAmbigRelocs {
         ambig_relocs.extend(self.get_ambiguous_relocations(
             config_path,
             &config.main_module,
-            symbol_maps.get(ModuleKind::Arm9).context("No symbol map found for main module")?,
+            &symbol_maps,
+            ModuleKind::Arm9,
         )?);
         for autoload in &config.autoloads {
-            let symbol_map = symbol_maps
-                .get(ModuleKind::Autoload(autoload.kind))
-                .context(format!("No symbol map found for autoload '{}'", autoload.kind))?;
-            ambig_relocs.extend(self.get_ambiguous_relocations(config_path, &autoload.module, symbol_map)?);
+            ambig_relocs.extend(self.get_ambiguous_relocations(
+                config_path,
+                &autoload.module,
+                &symbol_maps,
+                ModuleKind::Autoload(autoload.kind),
+            )?);
         }
         for overlay in &config.overlays {
-            let symbol_map = symbol_maps
-                .get(ModuleKind::Overlay(overlay.id))
-                .context(format!("No symbol map found for overlay {}", overlay.id))?;
-            ambig_relocs.extend(self.get_ambiguous_relocations(config_path, &overlay.module, symbol_map)?);
+            ambig_relocs.extend(self.get_ambiguous_relocations(
+                config_path,
+                &overlay.module,
+                &symbol_maps,
+                ModuleKind::Overlay(overlay.id),
+            )?);
         }
 
         Ok(ambig_relocs)
@@ -64,8 +75,10 @@ impl DumpAmbigRelocs {
         &self,
         config_path: &Path,
         module_config: &ConfigModule,
-        symbol_map: &SymbolMap,
+        symbol_maps: &SymbolMaps,
+        module_kind: ModuleKind,
     ) -> Result<Vec<RelocInfo>> {
+        let symbol_map = symbol_maps.get(module_kind).context(format!("No symbol map found for module {}", module_kind))?;
         let relocations = Relocations::from_file(config_path.join(&module_config.relocations))?;
         let infos = relocations
             .iter()
@@ -80,7 +93,7 @@ impl DumpAmbigRelocs {
                     .cloned();
                 let offset = if let Some(symbol) = &symbol { relocation.from_address() - symbol.addr } else { 0 };
 
-                Some(RelocInfo { relocation, symbol, offset })
+                Some(RelocInfo { module_kind, relocation, symbol, offset })
             })
             .collect();
         Ok(infos)
@@ -88,6 +101,7 @@ impl DumpAmbigRelocs {
 }
 
 struct RelocInfo {
+    module_kind: ModuleKind,
     relocation: Relocation,
     symbol: Option<Symbol>,
     offset: u32,
